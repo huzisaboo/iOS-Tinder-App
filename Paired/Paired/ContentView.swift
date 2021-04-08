751773a43a40aede32a203d253c9378b9ec55515
Huzaifa Saboowala <huzaifa.saboowala96@gmail.com>
2021-04-08 15:18:28 -0700

    Created Layout with one VStack and 2 HStacks

--- Paired/Paired/ContentView.swift
+++ Paired/Paired/ContentView.swift
@@ -9,8 +9,29 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack{
+            //Top Stack
+            HStack{
+                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                    Image("top_left_profile")
+                }
+                Button(action:{}){
+                    Image("tinder-icon").resizable().aspectRatio(contentMode: .fit)
+                        .frame(height:45)
+                }
+                Button(action:{}){
+                    
+                }
+            }
+            
+            //Card
+            RoundedRectangle(cornerRadius: 8.0)
+            
+            //Bottom Stack
+            HStack{
+                
+            }
+        }
     }
 }
 
