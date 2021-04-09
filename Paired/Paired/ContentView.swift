95d8a656e9666342be02dcfc72c2d208fad5ef37
Huzaifa Saboowala <huzaifa.saboowala96@gmail.com>
2021-04-08 23:17:33 -0700

    Implemented LIKE and NOPE display on swiping. Cards reset if not swiped enough or go in that direction if swiped. Left = Nope and Right = Like

--- Paired/Paired/ContentView.swift
+++ Paired/Paired/ContentView.swift
@@ -8,8 +8,6 @@
 import SwiftUI
 
 struct ContentView: View {
-    
-   
     var body: some View {
         VStack{
             //Top Stack
@@ -31,10 +29,16 @@ struct ContentView: View {
             
             //Card
             ZStack {
+                VStack{
+                    Text("All cards over!").font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/).fontWeight(.medium)
+                }
+                
                 ForEach(Card.data.reversed()) { card in
+                    
                     CardView(card: card).padding(8)
                 }
-            }
+                
+            }.zIndex(/*@START_MENU_TOKEN@*/1.0/*@END_MENU_TOKEN@*/)
             
             //Bottom Stack
             HStack(spacing: 0){
@@ -66,7 +70,7 @@ struct CardView: View {
     @State var card : Card
     let cardGradient = Gradient(colors: [Color.black.opacity(0),Color.black.opacity(0.5)])
     var body: some View {
-        ZStack(alignment: .leading) {
+        ZStack(alignment: .topLeading) {
             Image(card.imageName).resizable()
             LinearGradient(gradient: cardGradient, startPoint: .top, endPoint: .bottom)
             
@@ -81,6 +85,22 @@ struct CardView: View {
                     Text(card.bio).foregroundColor(Color.white).padding([.leading, .bottom])
                 }
             }
+            
+            HStack{
+                Image("like")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width:150)
+                    .opacity(Double(card.x / 10 - 1))
+                Spacer()
+                Image("nope")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width:150)
+                    .opacity(Double(card.x/10 * -1 - 1))
+            }
+            
+            
         }.cornerRadius(10)
         .offset(x: card.x, y: card.y)
         .rotationEffect(.init(degrees: card.degree))
@@ -101,6 +121,20 @@ struct CardView: View {
                 }
             
                 .onEnded{ value in
+                    withAnimation(.interpolatingSpring(mass: 1, stiffness: 50, damping: 8, initialVelocity: 0))
+                    {
+                        switch value.translation.width{
+                        case 0...100:
+                            card.x = 0; card.y = 0; card.degree = 0
+                        case let x where x > 100:
+                            card.x = 500; card.degree = 12
+                        case (-100)...(-1):
+                            card.x = 0; card.y = 0; card.degree = 0
+                        case let x where x < -100:
+                            card.x = -500; card.degree = -12
+                        default: card.x = 0 ; card.y = 0
+                        }
+                    }
                     
                 }
             
